04a1b395f7519d2571f88bd6440be409f869deb9
Muhammed Shimer <102233917+shimer6565@users.noreply.github.com>
2023-02-11 20:06:05 +0530

    updated relation logic

--- relations.py
+++ relations.py
@@ -1,4 +1,5 @@
 import psycopg2
+import scipy
 import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
@@ -26,24 +27,19 @@ try:
 
     cur = conn.cursor()
     
-    cur.execute('SELECT a.id,b.id from agent_info a INNER JOIN brokerage b on a.brokerage_id = b.id order by b.id asc')
-    agents_brokerage = cur.fetchmany(20)
+    #   QUERY TO CREATE AGENT_RELATIONS TABLE 
+    #   create table agent_relations as (select a.id as agent_list_id, h.id as home_id, b.id as agent_sell_id from agent_listing a inner join home_info h on a.home_id = h.id inner join agent_listing b on b.home_id = h.id where a.deal_side = "LISTING" and b.deal_side = "SELLING")
+    cur.execute('SELECT * FROM agent_relations')
+    agents_relations = cur.fetchmany(1000)
 
-    relations = []
-    for i in range(len(agents_brokerage)):
-        for j in range(i+1, len(agents_brokerage)):
-            if agents_brokerage[i][1] == agents_brokerage[j][1]:
-                relations.append([agents_brokerage[i][0], agents_brokerage[j][0]])
-            else:
-                break
-
-
+    
     graph = nx.Graph()
  
-    for relation in relations:
-        graph.add_edge(relation[0], relation[1])
+    for relation in agents_relations:
+        #print(relation[0], relation[2])
+        graph.add_edge(relation[0], relation[2])
     
-    nx.draw(graph)
+    nx.draw(graph, node_size=10)
     plt.savefig("relations.png")
     
 
